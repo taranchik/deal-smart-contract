b44d672e2925ab7188cab723b96746634c7be37b
Viacheslav Taranushenko <viacheslav.taranushenko@bbb.io>
2021-09-12 13:36:13 +0300

    Improved smart contract code

--- contracts/Deal.sol
+++ contracts/Deal.sol
@@ -9,19 +9,30 @@ contract Deal {
     mapping(uint => Complaint) private complains;
     mapping(uint => Invoice) private invoices;
     mapping(string => Product) private products;
+    mapping(string => ERC20Token) private ERC20Tokens;
     uint private complainsCount = 0;
     uint private invoicesCount = 0;
     
     constructor() {
-        products["Bike"] = Product({ price: 1, tokenAddress: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, isBroken: false, productOwner: msg.sender });
-        products["Car"] = Product({ price: 5, tokenAddress: 0xdAC17F958D2ee523a2206206994597C13D831ec7, isBroken: true, productOwner: msg.sender });
-        products["Rollers"] = Product({ price: 2, tokenAddress: 0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE, isBroken: false, productOwner: msg.sender });
-        products["Skateboard"] = Product({ price: 2, tokenAddress: address(0), isBroken: false, productOwner: msg.sender });
+        ERC20Tokens["USDC"] = ERC20Token({ token: ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48), tokenOwner: 0x95Ba4cF87D6723ad9C0Db21737D862bE80e93911 });
+        ERC20Tokens["DAI"] = ERC20Token({ token: ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F), tokenOwner: 0xdDb108893104dE4E1C6d0E47c42237dB4E617ACc });
+        ERC20Tokens["LINK"] = ERC20Token({ token: ERC20(0x514910771AF9Ca656af840dff83E8264EcF986CA), tokenOwner: 0xf55037738604FDDFC4043D12F25124E94D7D1780 });
+        ERC20Tokens["NULL"] = ERC20Token({ token: ERC20(address(0)), tokenOwner: address(0) });
+        
+        products["Bike"] = Product({ price: 1, token: ERC20Tokens["USDC"], isBroken: false, productOwner: msg.sender });
+        products["Car"] = Product({ price: 5, token: ERC20Tokens["DAI"], isBroken: true, productOwner: msg.sender });
+        products["Rollers"] = Product({ price: 2, token: ERC20Tokens["LINK"], isBroken: false, productOwner: msg.sender });
+        products["Skateboard"] = Product({ price: 2, token: ERC20Tokens["NULL"], isBroken: false, productOwner: msg.sender });
+    }
+    
+    struct ERC20Token {
+        ERC20 token;
+        address tokenOwner;
     }
     
     struct Product {
         uint price;
-        address tokenAddress;
+        ERC20Token token;
         bool isBroken;
         address productOwner;
     }
@@ -48,8 +59,8 @@ contract Deal {
        return (invoices[invoiceIndex].buyer, invoices[invoiceIndex].seller, invoices[invoiceIndex].productName, invoices[invoiceIndex].price);
    } 
    
-   function getProductInfo(string memory name) public view returns(uint, address, bool, address) {
-       return(products[name].price, products[name].tokenAddress, products[name].isBroken, products[name].productOwner);
+   function getProductInfo(string memory name) public view returns(uint, ERC20Token memory, bool, address) {
+       return(products[name].price, products[name].token, products[name].isBroken, products[name].productOwner);
    }
    
    function getComplaintInfo(uint complaintIndex) public view returns(Invoice memory, string memory, string memory, string memory, bool) {
@@ -62,70 +73,45 @@ contract Deal {
                 complains[complaintIndex].isResolved);
    }
    
-//   function exchangeOneEtherToERC20(address tokenAddress) public payable {
-//       require(msg.value == 1 ether, "Should be sent for exchanging exactly 1 ether");
-      
-//       ERC20 token = ERC20(tokenAddress);
-      
-//       token.approve(msg.sender, 1);
-//       token.transfer(msg.sender, 1);
-//     }
-   
-   // accepts Ether
-   function buyProductWithEther(string memory productName) payable public {
+     function buyProduct(string memory productName) payable public {
        require(products[productName].price != 0, "Purchase of a non-existent product is not possible");
        
        require(arbitrator != msg.sender, "The arbitrator can't buy a product");
        
        require(products[productName].productOwner != msg.sender, "The product owner can't buy his own product");
        
-       require(products[productName].price == msg.value, "There are enough funds to cover the cost of the product");
+       if(products[productName].token.tokenOwner == address(0)){
+           require(products[productName].price == msg.value, "The paid amount of Ether is different than the price of the product");
+           
+           invoices[invoicesCount] = Invoice({
+                buyer: msg.sender,
+                seller: payable(products[productName].productOwner),
+                productName: productName,
+                price: products[productName].price,
+                date: block.timestamp
+            });
        
-       require(products[productName].tokenAddress == address(0), "The product can not be pruchased for Ether");
-       
-       invoices[invoicesCount] = Invoice({
-        buyer: msg.sender,
-        seller: payable(products[productName].productOwner),
-        productName: productName,
-        price: products[productName].price,
-        date: block.timestamp
-    });
-       
-       products[productName].productOwner = msg.sender;
-       invoices[invoicesCount].seller.transfer(products[productName].price); // transfer eather from buyer to seller
-       invoicesCount++;
+            products[productName].productOwner = msg.sender;
+            invoices[invoicesCount].seller.transfer(products[productName].price); // transfer eather from buyer to seller
+            invoicesCount++;
+       } else{
+           require(products[productName].token.token.balanceOf(msg.sender) >= products[productName].price, "There are enough ERC20 tokens to cover the cost of the product");
+           
+           invoices[invoicesCount] = Invoice({
+                buyer: msg.sender,
+                seller: payable(products[productName].productOwner),
+                productName: productName,
+                price: products[productName].price,
+                date: block.timestamp
+            });
+        
+            products[productName].productOwner = msg.sender;
+            products[productName].token.token.approve(invoices[invoicesCount].buyer, products[productName].price);
+            products[productName].token.token.transferFrom(invoices[invoicesCount].buyer, invoices[invoicesCount].seller, products[productName].price); // transfer eather from buyer to seller
+            invoicesCount++;
+       }
     }
 
-   // accepts ERC20 tokens
-   function buyProductWithERC20Token(string memory productName, address tokenAddress) public {
-       require(address(tokenAddress) != address(0), "The address of the token can not be null");
-       
-       require(products[productName].price != 0, "Purchase of a non-existent product is not possible");
-       
-       require(products[productName].tokenAddress == tokenAddress, "The product is sold for a different token");
-       
-       require(arbitrator != msg.sender, "The arbitrator can't buy a product");
-       
-       require(products[productName].productOwner != msg.sender, "The product owner can't buy his own product");
-       
-       ERC20 token = ERC20(tokenAddress);
-       
-       require(token.balanceOf(msg.sender) >= products[productName].price, "There are enough funds to cover the cost of the product");
-    
-       invoices[invoicesCount] = Invoice({
-        buyer: msg.sender,
-        seller: payable(products[productName].productOwner),
-        productName: productName,
-        price: products[productName].price,
-        date: block.timestamp
-    });
-        
-        products[productName].productOwner = msg.sender;
-        token.approve(invoices[invoicesCount].buyer, products[productName].price);
-        token.transferFrom(invoices[invoicesCount].buyer, invoices[invoicesCount].seller, products[productName].price); // transfer eather from buyer to seller
-        invoicesCount++;
-    }
-    
     function makeComplaint(uint invoiceIndex, string memory comment) public {
         require(invoiceIndex < invoicesCount, "There is no invoice for this index");
         
@@ -167,4 +153,5 @@ contract Deal {
                     
         complains[complaintIndex].arbitratorComment = comment;
         complains[complaintIndex].isResolved = true;
+    }
 }
