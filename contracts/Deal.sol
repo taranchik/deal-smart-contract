6286f9e13e26cee374f7b5c299e0f6d3103c5100
Viacheslav Taranushenko <viacheslav.taranushenko@bbb.io>
2021-09-12 21:14:33 +0300

    Finished desired testing functionality

--- contracts/Deal.sol
+++ contracts/Deal.sol
@@ -6,32 +6,64 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Deal {
     address private arbitrator;
-    mapping(uint => Complaint) private complains;
-    mapping(uint => Invoice) private invoices;
+    mapping(uint256 => Complaint) public complains;
+    mapping(uint256 => Invoice) public invoices;
     mapping(string => Product) public products;
     mapping(string => ERC20Token) private ERC20Tokens;
-    uint private complainsCount = 0;
-    uint private invoicesCount = 0;
-    
+    uint256 public complainsCount = 0;
+    uint256 public invoicesCount = 0;
+
     constructor() {
-        ERC20Tokens["USDC"] = ERC20Token({ token: ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48), tokenOwner: 0x95Ba4cF87D6723ad9C0Db21737D862bE80e93911 });
-        ERC20Tokens["DAI"] = ERC20Token({ token: ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F), tokenOwner: 0xdDb108893104dE4E1C6d0E47c42237dB4E617ACc });
-        ERC20Tokens["LINK"] = ERC20Token({ token: ERC20(0x514910771AF9Ca656af840dff83E8264EcF986CA), tokenOwner: 0xf55037738604FDDFC4043D12F25124E94D7D1780 });
-        ERC20Tokens["NULL"] = ERC20Token({ token: ERC20(address(0)), tokenOwner: address(0) });
-        
-        products["Bike"] = Product({ price: 1, token: ERC20Tokens["USDC"], isBroken: false, productOwner: msg.sender });
-        products["Car"] = Product({ price: 5, token: ERC20Tokens["DAI"], isBroken: true, productOwner: msg.sender });
-        products["Rollers"] = Product({ price: 2, token: ERC20Tokens["LINK"], isBroken: false, productOwner: msg.sender });
-        products["Skateboard"] = Product({ price: 2, token: ERC20Tokens["NULL"], isBroken: false, productOwner: msg.sender });
+        ERC20Tokens["USDC"] = ERC20Token({
+            token: ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48),
+            tokenOwner: 0x95Ba4cF87D6723ad9C0Db21737D862bE80e93911
+        });
+        ERC20Tokens["DAI"] = ERC20Token({
+            token: ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F),
+            tokenOwner: 0xdDb108893104dE4E1C6d0E47c42237dB4E617ACc
+        });
+        ERC20Tokens["LINK"] = ERC20Token({
+            token: ERC20(0x514910771AF9Ca656af840dff83E8264EcF986CA),
+            tokenOwner: 0xf55037738604FDDFC4043D12F25124E94D7D1780
+        });
+        ERC20Tokens["NULL"] = ERC20Token({
+            token: ERC20(address(0)),
+            tokenOwner: address(0)
+        });
+
+        products["Bike"] = Product({
+            price: 1,
+            token: ERC20Tokens["USDC"],
+            isBroken: false,
+            productOwner: msg.sender
+        });
+        products["Car"] = Product({
+            price: 5,
+            token: ERC20Tokens["DAI"],
+            isBroken: true,
+            productOwner: msg.sender
+        });
+        products["Rollers"] = Product({
+            price: 2,
+            token: ERC20Tokens["LINK"],
+            isBroken: false,
+            productOwner: msg.sender
+        });
+        products["Skateboard"] = Product({
+            price: 2,
+            token: ERC20Tokens["NULL"],
+            isBroken: false,
+            productOwner: msg.sender
+        });
     }
-    
+
     struct ERC20Token {
         ERC20 token;
         address tokenOwner;
     }
-    
+
     struct Product {
-        uint price;
+        uint256 price;
         ERC20Token token;
         bool isBroken;
         address productOwner;
@@ -41,8 +73,8 @@ contract Deal {
         address buyer;
         address payable seller;
         string productName;
-        uint price;
-        uint date;
+        uint256 price;
+        uint256 date;
     }
 
     struct Complaint {
@@ -51,106 +83,194 @@ contract Deal {
         string sellerComment;
         string arbitratorComment;
         bool isResolved;
-   }
-   
-   function getInvoiceInfo(uint invoiceIndex) public view returns(address, address payable, string memory, uint) {
-       require(invoiceIndex < invoicesCount, "There is no invoice for this index");
-       
-       return (invoices[invoiceIndex].buyer, invoices[invoiceIndex].seller, invoices[invoiceIndex].productName, invoices[invoiceIndex].price);
-   } 
-   
-   function getProductInfo(string memory name) public view returns(uint, ERC20Token memory, bool, address) {
-       return(products[name].price, products[name].token, products[name].isBroken, products[name].productOwner);
-   }
-   
-   function getComplaintInfo(uint complaintIndex) public view returns(Invoice memory, string memory, string memory, string memory, bool) {
-       require(complaintIndex < complainsCount, "There is no complaint for this index");
-       
-       return(complains[complaintIndex].invoice, 
-            complains[complaintIndex].buyerComment,
-                complains[complaintIndex].sellerComment, 
-                complains[complaintIndex].arbitratorComment, 
-                complains[complaintIndex].isResolved);
-   }
-   
-     function buyProduct(string memory productName) payable public {
-       require(products[productName].price != 0, "Purchase of a non-existent product is not possible");
-       
-       require(arbitrator != msg.sender, "The arbitrator can't buy a product");
-       
-       require(products[productName].productOwner != msg.sender, "The product owner can't buy his own product");
-       
-       if(products[productName].token.tokenOwner == address(0)){
-           require(products[productName].price == msg.value, "The paid amount of Ether is different than the price of the product");
-           
-           invoices[invoicesCount] = Invoice({
-                buyer: msg.sender,
-                seller: payable(products[productName].productOwner),
-                productName: productName,
-                price: products[productName].price,
-                date: block.timestamp
-            });
-       
-            products[productName].productOwner = msg.sender;
-            invoices[invoicesCount].seller.transfer(products[productName].price); // transfer eather from buyer to seller
-            invoicesCount++;
-       } else{
-           require(products[productName].token.token.balanceOf(msg.sender) >= products[productName].price, "There are enough ERC20 tokens to cover the cost of the product");
-           
-           invoices[invoicesCount] = Invoice({
-                buyer: msg.sender,
-                seller: payable(products[productName].productOwner),
-                productName: productName,
-                price: products[productName].price,
-                date: block.timestamp
-            });
-        
-            products[productName].productOwner = msg.sender;
-            products[productName].token.token.approve(invoices[invoicesCount].buyer, products[productName].price);
-            products[productName].token.token.transferFrom(invoices[invoicesCount].buyer, invoices[invoicesCount].seller, products[productName].price); // transfer eather from buyer to seller
-            invoicesCount++;
-       }
     }
 
-    function makeComplaint(uint invoiceIndex, string memory comment) public {
-        require(invoiceIndex < invoicesCount, "There is no invoice for this index");
-        
-        require(invoices[invoiceIndex].buyer == msg.sender || 
-                invoices[invoiceIndex].seller == msg.sender, 
-                    "It is impossible to complain not yours invoice");
-                    
-        string memory buyerComment = '';
-        string memory sellerComment = '';
-        
-        if(msg.sender == invoices[invoiceIndex].buyer){
-            buyerComment = comment;
+    function getInvoiceInfo(uint256 invoiceIndex)
+        public
+        view
+        returns (
+            address,
+            address payable,
+            string memory,
+            uint256,
+            uint256
+        )
+    {
+        require(
+            invoiceIndex < invoicesCount,
+            "There is no invoice for this index"
+        );
+
+        return (
+            invoices[invoiceIndex].buyer,
+            invoices[invoiceIndex].seller,
+            invoices[invoiceIndex].productName,
+            invoices[invoiceIndex].price,
+            invoices[invoiceIndex].date
+        );
+    }
+
+    function getProductInfo(string memory name)
+        public
+        view
+        returns (
+            uint256,
+            ERC20Token memory,
+            bool,
+            address
+        )
+    {
+        return (
+            products[name].price,
+            products[name].token,
+            products[name].isBroken,
+            products[name].productOwner
+        );
+    }
+
+    function getComplaintInfo(uint256 complaintIndex)
+        public
+        view
+        returns (
+            Invoice memory,
+            string memory,
+            string memory,
+            string memory,
+            bool
+        )
+    {
+        require(
+            complaintIndex < complainsCount,
+            "There is no complaint for this index"
+        );
+
+        return (
+            complains[complaintIndex].invoice,
+            complains[complaintIndex].buyerComment,
+            complains[complaintIndex].sellerComment,
+            complains[complaintIndex].arbitratorComment,
+            complains[complaintIndex].isResolved
+        );
+    }
+
+    function buyProduct(string memory productName) public payable {
+        require(
+            products[productName].price != 0,
+            "Purchase of a non-existent product is not possible"
+        );
+
+        require(arbitrator != msg.sender, "The arbitrator can't buy a product");
+
+        require(
+            products[productName].productOwner != msg.sender,
+            "The product owner can't buy his own product"
+        );
+
+        if (products[productName].token.tokenOwner == address(0)) {
+            require(
+                products[productName].price == msg.value,
+                "The paid amount of Ether is different than the price of the product"
+            );
+
+            invoices[invoicesCount] = Invoice({
+                buyer: msg.sender,
+                seller: payable(products[productName].productOwner),
+                productName: productName,
+                price: products[productName].price,
+                date: block.timestamp
+            });
+
+            products[productName].productOwner = msg.sender;
+            invoices[invoicesCount].seller.transfer(
+                products[productName].price
+            ); // transfer eather from buyer to seller
+            invoicesCount++;
+        } else {
+            require(
+                products[productName].token.token.balanceOf(msg.sender) >=
+                    products[productName].price,
+                "There are enough ERC20 tokens to cover the cost of the product"
+            );
+
+            invoices[invoicesCount] = Invoice({
+                buyer: msg.sender,
+                seller: payable(products[productName].productOwner),
+                productName: productName,
+                price: products[productName].price,
+                date: block.timestamp
+            });
+
+            products[productName].productOwner = msg.sender;
+            products[productName].token.token.approve(
+                invoices[invoicesCount].buyer,
+                products[productName].price
+            );
+            products[productName].token.token.transferFrom(
+                invoices[invoicesCount].buyer,
+                invoices[invoicesCount].seller,
+                products[productName].price
+            ); // transfer eather from buyer to seller
+            invoicesCount++;
         }
-        else{
+    }
+
+    function makeComplaint(uint256 invoiceIndex, string memory comment) public {
+        require(
+            invoiceIndex < invoicesCount,
+            "There is no invoice for this index"
+        );
+
+        require(
+            invoices[invoiceIndex].buyer == msg.sender ||
+                invoices[invoiceIndex].seller == msg.sender,
+            "It is impossible to complain not yours invoice"
+        );
+
+        string memory buyerComment = "";
+        string memory sellerComment = "";
+
+        if (msg.sender == invoices[invoiceIndex].buyer) {
+            buyerComment = comment;
+        } else {
             sellerComment = comment;
         }
-                    
+
         complains[complainsCount] = Complaint({
             invoice: invoices[invoiceIndex],
             buyerComment: buyerComment,
             sellerComment: sellerComment,
-            arbitratorComment: '',
+            arbitratorComment: "",
             isResolved: false
         });
 
         complainsCount++;
     }
-    
-    function resolveComplaint(uint complaintIndex, string memory comment) public {
-        require(complaintIndex < complainsCount, "There is no complaint for this index");
-        
-        require(complains[complaintIndex].invoice.buyer != msg.sender, "Buyer can not resolve complaints");
-        
-        require(complains[complaintIndex].invoice.seller != msg.sender, "Seller can not resolve complaints");
-                   
-        require(complains[complaintIndex].isResolved != true, "Only unresolved complaints is possible to resolve");
-        
+
+    function resolveComplaint(uint256 complaintIndex, string memory comment)
+        public
+    {
+        require(
+            complaintIndex < complainsCount,
+            "There is no complaint for this index"
+        );
+
+        require(
+            complains[complaintIndex].invoice.buyer != msg.sender,
+            "Buyer can not resolve complaints"
+        );
+
+        require(
+            complains[complaintIndex].invoice.seller != msg.sender,
+            "Seller can not resolve complaints"
+        );
+
+        require(
+            complains[complaintIndex].isResolved != true,
+            "Only unresolved complaints is possible to resolve"
+        );
+
         arbitrator = msg.sender;
-                    
+
         complains[complaintIndex].arbitratorComment = comment;
         complains[complaintIndex].isResolved = true;
     }
--- test/Deal.test.js
+++ test/Deal.test.js
@@ -22,60 +22,366 @@ contract("Deal", function (accounts) {
     instance = null;
   });
 
-  describe("Testing of possible functionality", function () {
+  describe("Testing of possible functionality", () => {
     it("buyer should buy a product with Ether", async () => {
       // setup
-      let beforeProductOwner, afterProductOwner;
+      let beforeProductOwner, afterProductOwner, productPrice;
+      let key = "Skateboard";
 
       // exercise
       beforeProductOwner = await instance
-        .products("Skateboard")
+        .products(key)
         .then((result) => result.productOwner);
       productPrice = await instance
-        .products("Skateboard")
+        .products(key)
         .then((result) => result.price.toNumber());
-      await instance.buyProduct("Skateboard", {
+      await instance.buyProduct(key, {
         from: buyer,
         gas: 200000,
         value: productPrice,
       });
       afterProductOwner = await instance
-        .products("Skateboard")
+        .products(key)
         .then((result) => result.productOwner);
-
-      console.log(beforeProductOwner, afterProductOwner);
 
       // verify
       assert(beforeProductOwner !== afterProductOwner);
     });
 
-    it("buyer should buy a product with Ether", async () => {
+    it("buyer should make a complaint on a product", async () => {
       // setup
-      let beforeProductOwner, afterProductOwner;
+      let beforeComplainsCount, afterComplainsCount;
+      let index = 0;
 
       // exercise
-      beforeProductOwner = await instance
-        .products("Skateboard")
-        .then((result) => result.productOwner);
-      productPrice = await instance
-        .products("Skateboard")
-        .then((result) => result.price.toNumber());
-      await instance.buyProduct("Skateboard", {
-        from: buyer,
-        gas: 200000,
-        value: productPrice,
-      });
-      afterProductOwner = await instance
-        .products("Skateboard")
-        .then((result) => result.productOwner);
+      beforeComplainsCount = await instance.complainsCount();
 
-      console.log(beforeProductOwner, afterProductOwner);
+      await instance.makeComplaint(index, "Test comment from buyer complaint", {
+        from: buyer,
+      });
+      afterComplainsCount = await instance.complainsCount();
 
       // verify
-      assert(beforeProductOwner !== afterProductOwner);
+      assert(beforeComplainsCount.toNumber() < afterComplainsCount.toNumber());
+    });
+
+    it("arbitrator should resolve a complaint from buyer on a product", async () => {
+      // setup
+      let beforeComplainsCount, afterComplainsCount, complains;
+      let index = 0;
+
+      // exercise
+      beforeComplaintResolution = await instance
+        .complains(index.toString())
+        .then((result) => result.isResolved);
+      await instance.resolveComplaint(
+        index,
+        "Test comment from arbitrator for the buyer complaint",
+        {
+          from: arbitrator,
+        }
+      );
+      afterComplaintResolution = await instance
+        .complains(index.toString())
+        .then((result) => result.isResolved);
+
+      // verify
+      assert(!beforeComplaintResolution && afterComplaintResolution);
+    });
+
+    it("seller should make a complaint on a product", async () => {
+      // setup
+      let beforeComplainsCount, afterComplainsCount;
+      let index = 0;
+
+      // exercise
+      beforeComplainsCount = await instance.complainsCount();
+
+      await instance.makeComplaint(
+        index,
+        "Test comment from seller complaint",
+        {
+          from: seller,
+        }
+      );
+      afterComplainsCount = await instance.complainsCount();
+
+      // verify
+      assert(beforeComplainsCount.toNumber() < afterComplainsCount.toNumber());
+    });
+
+    it("arbitrator should resolve a complaint from seller on a product", async () => {
+      // setup
+      let beforeComplainsCount, afterComplainsCount, complains;
+      let index = 1;
+
+      // exercise
+      beforeComplaintResolution = await instance
+        .complains(index.toString())
+        .then((result) => result.isResolved);
+      await instance.resolveComplaint(
+        index,
+        "Test comment from arbitrator for the seller complaint",
+        {
+          from: arbitrator,
+        }
+      );
+      afterComplaintResolution = await instance
+        .complains(index.toString())
+        .then((result) => result.isResolved);
+
+      // verify
+      assert(!beforeComplaintResolution && afterComplaintResolution);
+    });
+
+    it("arbitrator should receive information about the invoice", async () => {
+      // setup
+      let _invoiceInfo;
+      let index = 0;
+
+      // exercise
+      let expectedResult = {
+        0: await instance
+          .invoices(index.toString())
+          .then((result) => result.buyer),
+        1: await instance
+          .invoices(index.toString())
+          .then((result) => result.seller),
+        2: await instance
+          .invoices(index.toString())
+          .then((result) => result.productName),
+        3: await instance
+          .invoices(index.toString())
+          .then((result) => parseInt(result.price)),
+        4: await instance
+          .invoices(index.toString())
+          .then((result) => parseInt(result.date)),
+      };
+
+      _invoiceInfo = await instance.getInvoiceInfo(index, { from: arbitrator });
+      _invoiceInfo["3"] = _invoiceInfo["3"].toNumber();
+      _invoiceInfo["4"] = _invoiceInfo["4"].toNumber();
+
+      // verify
+      expect(_invoiceInfo).to.eql(expectedResult);
+    });
+
+    it("buyer should receive information about the invoice", async () => {
+      // setup
+      let _invoiceInfo;
+      let index = 0;
+
+      // exercise
+      let expectedResult = {
+        0: await instance
+          .invoices(index.toString())
+          .then((result) => result.buyer),
+        1: await instance
+          .invoices(index.toString())
+          .then((result) => result.seller),
+        2: await instance
+          .invoices(index.toString())
+          .then((result) => result.productName),
+        3: await instance
+          .invoices(index.toString())
+          .then((result) => parseInt(result.price)),
+        4: await instance
+          .invoices(index.toString())
+          .then((result) => parseInt(result.date)),
+      };
+
+      _invoiceInfo = await instance.getInvoiceInfo(index, { from: buyer });
+      _invoiceInfo["3"] = _invoiceInfo["3"].toNumber();
+      _invoiceInfo["4"] = _invoiceInfo["4"].toNumber();
+
+      // verify
+      expect(_invoiceInfo).to.eql(expectedResult);
+    });
+
+    it("seller should receive information about the invoice", async () => {
+      // setup
+      let _invoiceInfo;
+      let index = 0;
+
+      // exercise
+      let expectedResult = {
+        0: await instance
+          .invoices(index.toString())
+          .then((result) => result.buyer),
+        1: await instance
+          .invoices(index.toString())
+          .then((result) => result.seller),
+        2: await instance
+          .invoices(index.toString())
+          .then((result) => result.productName),
+        3: await instance
+          .invoices(index.toString())
+          .then((result) => parseInt(result.price)),
+        4: await instance
+          .invoices(index.toString())
+          .then((result) => parseInt(result.date)),
+      };
+
+      _invoiceInfo = await instance.getInvoiceInfo(index, { from: seller });
+      _invoiceInfo["3"] = _invoiceInfo["3"].toNumber();
+      _invoiceInfo["4"] = _invoiceInfo["4"].toNumber();
+
+      // verify
+      expect(_invoiceInfo).to.eql(expectedResult);
+    });
+
+    it("arbitrator should receive information about the product", async () => {
+      // setup
+      let _productInfo;
+      let key = "Skateboard";
+
+      // exercise
+      let expectedResult = {
+        0: await instance
+          .products(key)
+          .then((result) => parseInt(result.price)),
+        1: await instance.products(key).then((result) => result.token),
+        2: await instance.products(key).then((result) => result.isBroken),
+        3: await instance.products(key).then((result) => result.productOwner),
+      };
+
+      _productInfo = await instance.getProductInfo(key, { from: arbitrator });
+      _productInfo["0"] = _productInfo["0"].toNumber();
+
+      // verify
+      expect(_productInfo).to.eql(expectedResult);
+    });
+
+    it("buyer should receive information about the product", async () => {
+      // setup
+      let _productInfo;
+      let key = "Skateboard";
+
+      // exercise
+      let expectedResult = {
+        0: await instance
+          .products(key)
+          .then((result) => parseInt(result.price)),
+        1: await instance.products(key).then((result) => result.token),
+        2: await instance.products(key).then((result) => result.isBroken),
+        3: await instance.products(key).then((result) => result.productOwner),
+      };
+
+      _productInfo = await instance.getProductInfo(key, { from: buyer });
+      _productInfo["0"] = _productInfo["0"].toNumber();
+
+      // verify
+      expect(_productInfo).to.eql(expectedResult);
+    });
+
+    it("seller should receive information about the product", async () => {
+      // setup
+      let _productInfo;
+      let key = "Skateboard";
+
+      // exercise
+      let expectedResult = {
+        0: await instance
+          .products(key)
+          .then((result) => parseInt(result.price)),
+        1: await instance.products(key).then((result) => result.token),
+        2: await instance.products(key).then((result) => result.isBroken),
+        3: await instance.products(key).then((result) => result.productOwner),
+      };
+
+      _productInfo = await instance.getProductInfo(key, { from: seller });
+      _productInfo["0"] = _productInfo["0"].toNumber();
+
+      // verify
+      expect(_productInfo).to.eql(expectedResult);
+    });
+
+    it("arbitrator should receive information about the complaint", async () => {
+      // setup
+      let _productInfo;
+      let index = 0;
+
+      // exercise
+      let expectedResult = {
+        0: await instance.complains(index).then((result) => result.invoice),
+        1: await instance
+          .complains(index)
+          .then((result) => result.buyerComment),
+        2: await instance
+          .complains(index)
+          .then((result) => result.sellerComment),
+        3: await instance
+          .complains(index)
+          .then((result) => result.arbitratorComment),
+        4: await instance.complains(index).then((result) => result.isResolved),
+      };
+
+      _complaint = await instance.getComplaintInfo(index, {
+        from: arbitrator,
+      });
+
+      // verify
+      expect(_complaint).to.eql(expectedResult);
+    });
+
+    it("buyer should receive information about the complaint", async () => {
+      // setup
+      let _productInfo;
+      let index = 0;
+
+      // exercise
+      let expectedResult = {
+        0: await instance.complains(index).then((result) => result.invoice),
+        1: await instance
+          .complains(index)
+          .then((result) => result.buyerComment),
+        2: await instance
+          .complains(index)
+          .then((result) => result.sellerComment),
+        3: await instance
+          .complains(index)
+          .then((result) => result.arbitratorComment),
+        4: await instance.complains(index).then((result) => result.isResolved),
+      };
+
+      _complaint = await instance.getComplaintInfo(index, {
+        from: buyer,
+      });
+
+      // verify
+      expect(_complaint).to.eql(expectedResult);
+    });
+
+    it("seller should receive information about the complaint", async () => {
+      // setup
+      let _productInfo;
+      let index = 0;
+
+      // exercise
+      let expectedResult = {
+        0: await instance.complains(index).then((result) => result.invoice),
+        1: await instance
+          .complains(index)
+          .then((result) => result.buyerComment),
+        2: await instance
+          .complains(index)
+          .then((result) => result.sellerComment),
+        3: await instance
+          .complains(index)
+          .then((result) => result.arbitratorComment),
+        4: await instance.complains(index).then((result) => result.isResolved),
+      };
+
+      _complaint = await instance.getComplaintInfo(index, {
+        from: seller,
+      });
+
+      // verify
+      expect(_complaint).to.eql(expectedResult);
     });
   });
 
+  describe("Testing of unpossible functionality", () => {});
   // await Deal.deployed();
   // return assert.isTrue(true);
 });
